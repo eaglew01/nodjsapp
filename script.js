01c1d5519f12bba61ad65bd98d517df75da8d55e
eaglew01 <wouter.de.cleer@student.ehb.be>
2023-10-08 10:40:52 +0200

    connection to database

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const mongoose = require('mongoose');
 
+const app = express()
+mongoose
+.connect('mongodb+srv://wouterdecleer258:HjdRYLqLBBH3Rwuo@clusterfornodejsapp.0cervla.mongodb.net/NodeJSApp?retryWrites=true&w=majority')
+.then(() =>{
+    console.log("Database is connected")
+    app.listen(3000, ()=> {
+        console.log("Node API runs on port 3000")
+    })
+})
+.catch((Error) =>{
+    console.log(Error)
+})
 app.get('/test', function (req, res) {
   res.send('Hello World2')
 })
 
-app.listen(3000)
